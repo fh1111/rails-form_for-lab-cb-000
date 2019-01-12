416609f0d519b0f534e9207e045855e5b0053691
Fer Hernandez <fh.1111@gmail.com>
2019-01-12 18:19:08 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -4,5 +4,9 @@ class StudentsController < ApplicationController
     @student = Student.new
   end
 
-
+  def create
+     @student = Student.new(params.require(:student))
+     @student.save
+     redirect_to student_path(@student)
+   end
 end
